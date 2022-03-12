b75ec380757a0af01bcd1f094f854e5a10618c03
nicolasbuitragob <nicolasbuitragobautista@gmail.com>
2022-03-12 14:00:54 -0500

    pipeline training

---
+++ src/utils.py
@@ -0,0 +1,26 @@
+from sklearn.pipeline import Pipeline
+from joblib import dump
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+def update_model(model:Pipeline)-> None:
+    dump(model,'model/model.pkl')
+
+def save_simple_metrics_reports(train_score: float, test_score: float, validation_score: float, model:Pipeline)-> None:
+    with open('report.txt','w') as report_file:
+        report_file.write('#Model Pipeline Description\n')
+        for key, value in model.named_steps.items():
+            report_file.write(f'### {key}:{value.__repr__()}'+ '\n')
+        report_file.write(f'## Train Score: {train_score}\n')
+        report_file.write(f'## Validation Score: {validation_score}\n')
+        report_file.write(f'## Test Score: {test_score}\n')
+
+def get_model_performance_test_set(y_test:pd.Series, y_test_pred:pd.Series) -> None:
+    fig, ax = plt.subplots()
+    fig.set_figheight(8)
+    fig.set_figwidth(8)
+    sns.regplot(x = y_test_pred, y = y_test)
+    ax.set_xlabel('Predicted worldwide gross')
+    ax.set_ylabel('Real worldwide gross')
+    ax.set_title('Behavior of model prediction')
+    fig.savefig('prediction_behavior.png')
